4bde09ed6654ce007a2014febb1f29395ba65b34
nelsondevweb <butarbutarn1981@gmail.com>
2019-04-15 17:18:01 +0700

    djjdjd

--- apptestpythonanywhere/wsgi.py
+++ apptestpythonanywhere/wsgi.py
@@ -7,10 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apptestpythonanywhere.settings')
+#
+# application = get_wsgi_application()
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
 import os
+import sys
 
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/nelsonpythondev10981/apptestpythonanywhere'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'apptestpythonanywhere.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apptestpythonanywhere.settings')
-
 application = get_wsgi_application()
+###### or, for older Django <=1.4
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
